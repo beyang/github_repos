64b19a6ecbadfc90a961273291eb5a5708ab4205
Beyang Liu <beyang@sourcegraph.com>
2018-03-18 21:53:50 -0700

    .

--- fetch.go
+++ fetch.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -32,25 +33,33 @@ func run() {
 					langChans[lang] <- struct{}{}
 					defer func() { <-langChans[lang] }()
 
-					log.Printf("Querying %s, %d - %d", lang, i+1, i+100)
+					start, end := i*100+1, (i+1)*100
+					outfile := fmt.Sprintf("%s_%d-%d.txt", lang, start, end)
+					if _, err := os.Stat(outfile); err == nil { // Skip files that already exist
+						return
+					}
+
+					log.Printf("Querying %s, %d - %d", lang, start, end)
 					args := []string{"-H", `Authorization: token 3be14d5fad7e7889485c40987d5185c2d21a187b`, `-XGET`,
 						`https://api.github.com/search/repositories?s=stars&o=desc&q=language%3A` + lang + `+stars%3A<5000&type=Repositories&per_page=100&page=` + strconv.Itoa(i+1)}
 					b, err := exec.Command("curl", args...).Output()
 					if err != nil {
 						log.Fatal(err)
 					}
+
 					var r ReposResult
 					if err := json.Unmarshal(b, &r); err != nil {
 						log.Fatal(err)
 					}
+
 					repoNames := make([]string, 0, 100)
 					for _, item := range r.Items {
 						repoNames = append(repoNames, item.FullName)
 					}
-					if err := ioutil.WriteFile(fmt.Sprintf("%s_%d-%d.txt", lang, i+1, i+100), []byte(strings.Join(repoNames, "\n")), 0644); err != nil {
+					if err := ioutil.WriteFile(outfile, []byte(strings.Join(repoNames, "\n")), 0644); err != nil {
 						log.Fatal(err)
 					}
-					log.Printf("...Queried %s, %d - %d", lang, i+1, i+100)
+					log.Printf("...Queried %s, %d - %d", lang, start, end)
 				}(i)
 			}
 		}(lang)
