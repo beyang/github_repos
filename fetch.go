3c7491631b55847d1d863ee9b9fcdab4b2e5e9f7
Beyang Liu <beyang@sourcegraph.com>
2018-03-19 20:54:39 -0700

    .

--- fetch.go
+++ fetch.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type ReposResult struct {
@@ -21,45 +21,48 @@ type Repo struct {
 	FullName string `json:"full_name,omitempty"`
 }
 
-var (
-	langs     = []string{"go", "java", "python"}
-	langChans = map[string]chan struct{}{
-		"go":     make(chan struct{}, 10),
-		"java":   make(chan struct{}, 10),
-		"python": make(chan struct{}, 10),
-	}
-)
+var limiter = make(chan struct{}, 10)
+
+func init() {
+	go func() {
+		for {
+			limiter <- struct{}{}
+			time.Sleep(time.Second)
+		}
+	}()
+}
 
 func run() {
+	var wg sync.WaitGroup
+	langs := []string{"go", "java", "python"}
+	wg.Add(len(langs))
 	for _, lang := range langs {
 		go func(lang string) {
+			defer wg.Done()
+
 			maxStars := 5000
 			for i := 0; i < 10; i++ {
-				maxStars = traunch(lang, maxStars)
+				maxStars = traunch(lang, maxStars, i)
 				if maxStars == 0 {
 					break
 				}
 			}
 		}(lang)
 	}
-	select {}
+	wg.Wait()
 }
 
-func traunch(lang string, maxStars int) (nextMaxStars int) {
+func traunch(lang string, maxStars int, iter int) (nextMaxStars int) {
 	var wg sync.WaitGroup
 	wg.Add(10)
+
 	for i := 0; i < 10; i++ { // get 1000
 		go func(i int) {
 			defer wg.Done()
+			<-limiter
 
-			langChans[lang] <- struct{}{}
-			defer func() { <-langChans[lang] }()
-
-			start, end := i*100+1, (i+1)*100
-			outfile := fmt.Sprintf("%s_%d-%d.txt", lang, start, end)
-			if _, err := os.Stat(outfile); err == nil { // Skip files that already exist
-				return
-			}
+			start, end := (iter*1000)+i*100+1, (iter*1000)+(i+1)*100
+			outfile := fmt.Sprintf("data/%s_%d-%d.txt", lang, start, end)
 
 			log.Printf("Querying %s, %d - %d", lang, start, end)
 			args := []string{"-H", `Authorization: token 3be14d5fad7e7889485c40987d5185c2d21a187b`, `-XGET`,
@@ -75,6 +78,10 @@ func traunch(lang string, maxStars int) (nextMaxStars int) {
 			}
 
 			if i == 9 {
+				if len(r.Items) == 0 {
+					log.Printf("aborting lang %s on iter %d, i %d; last response: %s", lang, iter, i, string(b))
+					return
+				}
 				nextMaxStars = r.Items[len(r.Items)-1].Stars
 			}
 
@@ -88,6 +95,7 @@ func traunch(lang string, maxStars int) (nextMaxStars int) {
 			log.Printf("...Queried %s, %d - %d", lang, start, end)
 		}(i)
 	}
+
 	wg.Wait()
 	return
 }
